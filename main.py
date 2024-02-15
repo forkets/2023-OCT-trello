2713ece498e9e5ce2b5431f03748531087d48d7d
forkets <forkets@gmail.com>
2024-02-15 20:57:04 +1100

    ordered in card schema, and get all cards in card control

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from init import db, ma, bcrypt, jwt
 def create_app():
     app = Flask(__name__)
 
+    app.json.sort_keys = False
+
     # configs
     app.config["SQLALCHEMY_DATABASE_URI"]="postgresql+psycopg2://trello_dev:123456@localhost:5432/trello_db"
     app.config["JWT_SECRET_KEY"]="secret"
@@ -20,5 +22,8 @@ def create_app():
 
     from controllers.auth_controller import auth_bp
     app.register_blueprint(auth_bp)
+
+    from controllers.card_controller import cards_bp
+    app.register_blueprint(cards_bp)
 
     return app
--- models/card.py
+++ models/card.py
@@ -1,13 +1,14 @@
 from init import db, ma
 from marshmallow import fields
 
+
 class Card(db.Model):
     __tablename__ = "cards"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
-    date = db.Column(db.Date) # date the card was created
+    date = db.Column(db.Date)  # date the card was created
     status = db.Column(db.String)
     priority = db.Column(db.String)
 
@@ -25,10 +26,14 @@ class Card(db.Model):
     #   }
     # }
 
-    class CardSchema(ma.Schema):
-        user = fields.Nested('UserSchema', only = ['name', 'email'])
-        class Meta:
-            fields = ('id', 'title', 'description', 'date', 'status', 'priority', 'user')
+class CardSchema(ma.Schema):
+    user = fields.Nested('UserSchema', only=['name', 'email'])
 
-    card_schema = CardSchema()
-    cards_schema = CardSchema(many=True)
+    class Meta:
+        fields = ('id', 'title', 'description',
+                    'date', 'status', 'priority', 'user')
+        ordered = True
+
+
+card_schema = CardSchema()
+cards_schema = CardSchema(many=True)
